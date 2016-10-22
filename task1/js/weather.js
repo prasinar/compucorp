5b83ffc1e740ae2ee4406198ee00ac476b76e32e
prasinar <furtula@live.com>
2016-10-22 18:48:04 +0200

    adding comments

--- task1/js/weather.js
+++ task1/js/weather.js
@@ -1,13 +1,15 @@
 (function() {
 	var app = angular.module("app", ['ngSanitize']);
 
-	//var message = document.getElementById("message");
 	app.controller("controller", function($scope, $http) {
+		//init
 		$scope.message="Would you give us permission to access your location?";
 		$scope.messageShow=true;
 		$scope.yesNoButtonsShow=true;
 		$scope.topShow=true;
 		$scope.mainShow=false;
+
+		//if access to location is allowed
 		$scope.saidYes = function() {
 			$scope.message="Acquiring information...";
 			$scope.yesNoButtonsShow=false;
@@ -15,15 +17,21 @@
 			getLocation();
 			$scope.mainShow=true;
 		}
+
+		//if access to location is not allowed
 		$scope.saidNo = function() {
 			$scope.messageShow=false;
 			$scope.yesNoButtonsShow=false;
 			$scope.searchFormShow=true;
 		}
+
+		//displaying weather by value in text input
 		$scope.seeWeather = function() {
 			$scope.message="Acquiring information...";
 			$scope.messageShow=true;
 			$scope.topShow=false;
+
+			//if it is not a number (city)
 			if (isNaN($scope.location))
 			{
 				$http.get("http://api.openweathermap.org/data/2.5/find?q="+$scope.location+"&units=metric&type=like&APPID=71d17000e5ffe3100da7f1b8a7da36cb").then(function(response) {
@@ -39,6 +47,7 @@
 					$scope.mainShow=true;
 				});
 			}
+			//else if it is number (zip code)
 			else
 			{
 				$http.get("http://api.openweathermap.org/data/2.5/weather?zip="+$scope.location+"&units=metric&APPID=71d17000e5ffe3100da7f1b8a7da36cb").then(function(response) {
@@ -56,6 +65,8 @@
 			}
 			//api.openweathermap.org/data/2.5/weather?zip=94040
 		}
+
+		//asking for permission for location
 		function getLocation() {
 		    if (navigator.geolocation) {
 		        navigator.geolocation.getCurrentPosition(showPosition, showError);
@@ -64,6 +75,7 @@
 		    }
 		}
 
+		//if permission allowed show weather
 		function showPosition(position) {
 			$http.get("http://api.openweathermap.org/data/2.5/weather?lat="+position.coords.latitude+"&lon="+position.coords.longitude+
 		    				"&units=metric&APPID=71d17000e5ffe3100da7f1b8a7da36cb").then(function(response) {
@@ -79,6 +91,7 @@
 			});
 		}
 
+		//geolocation errors
 		function showError(error) {
 			switch(error.code) {
 			    case error.PERMISSION_DENIED:
